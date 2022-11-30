fd082c561fc2eaa1056c8c0bf3092b47647d8404
gersom911 <familiacedram@gmail.com>
2022-11-30 10:05:13 -0500

    tarea2

---
+++ priyecto1/tarea2.js
@@ -0,0 +1,24 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)*/
+
+let nombre = "Gersom Cedeño"
+let edad = 43
+let eres_un_desarrollador = true
+const fecha_nacimiento = new Date( "october,15,1979")
+libro ={
+        titulo: "El psicoanalista",
+        autor: "John Katzenbach", 
+        fecha: 2003,
+        url: "https://es.wikipedia.org/wiki/El_psicoanalista"
+}
+
+
